3194a3916c4481855f2f056bff2ce91f1f426ab7
heather7283 <heather7283@protonmail.com>
2025-03-07 16:45:04 +0400

    style: blank lines around includes and defines

--- src/stb_ds_impl.c
+++ src/stb_ds_impl.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
+
 #include "xmalloc.h"
-#define STBDS_REALLOC(context,ptr,size) xrealloc(ptr,size)
-#define STBDS_FREE(context,ptr) free(ptr)
+
+#define STBDS_REALLOC(context, ptr, size) xrealloc(ptr, size)
+#define STBDS_FREE(context, ptr) free(ptr)
 #define STBDS_NO_SHORT_NAMES
 #define STB_DS_IMPLEMENTATION
+
 #include "thirdparty/stb_ds.h"
 
