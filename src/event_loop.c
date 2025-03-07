85dceb1625f104031005c2a892f92be4e7c2b523
heather7283 <heather7283@protonmail.com>
2025-03-08 03:53:45 +0400

    refactor(event_loop): better way to add unconditional callbacks

--- src/event_loop.c
+++ src/event_loop.c
@@ -18,15 +18,9 @@ struct event_loop_item {
     struct event_loop *loop;
 
     int fd;
+    int priority;
     void *data;
     event_loop_callback_t callback;
-
-    struct spa_list link;
-};
-
-struct event_loop_uncond {
-    void *data;
-    event_loop_uncond_callback_t callback;
 
     struct spa_list link;
 };
@@ -37,7 +31,7 @@ struct event_loop {
     int epoll_fd;
 
     struct spa_list items;
-    struct spa_list uncond_callbacks;
+    struct spa_list unconditional_items;
 };
 
 static bool fd_is_valid(int fd) {
@@ -53,7 +47,7 @@ struct event_loop *event_loop_create(void) {
     struct event_loop *loop = xcalloc(1, sizeof(*loop));
 
     spa_list_init(&loop->items);
-    spa_list_init(&loop->uncond_callbacks);
+    spa_list_init(&loop->unconditional_items);
 
     loop->epoll_fd = epoll_create1(EPOLL_CLOEXEC);
     if (loop->epoll_fd < 0) {
@@ -76,9 +70,7 @@ void event_loop_cleanup(struct event_loop *loop) {
     spa_list_for_each_safe(item, item_tmp, &loop->items, link) {
         event_loop_remove_item(item);
     }
-
-    struct event_loop_uncond *callback, *callback_tmp;
-    spa_list_for_each_safe(callback, callback_tmp, &loop->uncond_callbacks, link) {
+    spa_list_for_each_safe(item, item_tmp, &loop->unconditional_items, link) {
         event_loop_remove_item(item);
     }
 
@@ -89,43 +81,87 @@ void event_loop_cleanup(struct event_loop *loop) {
 
 struct event_loop_item *event_loop_add_item(struct event_loop *loop, int fd,
                                             event_loop_callback_t callback, void *data) {
-    debug("event loop: adding fd %d to event loop", fd);
+    struct event_loop_item *new_item = NULL;
 
-    struct event_loop_item *new_item = xcalloc(1, sizeof(*new_item));
-    new_item->loop = loop;
-    new_item->fd = fd;
-    new_item->callback = callback;
-    new_item->data = data;
+    if (fd >= 0) {
+        debug("event loop: adding fd %d to event loop", fd);
 
-    struct epoll_event epoll_event;
-    epoll_event.events = EPOLLIN;
-    epoll_event.data.fd = fd;
-    if (epoll_ctl(loop->epoll_fd, EPOLL_CTL_ADD, fd, &epoll_event) < 0) {
-        int ret = errno;
-        err("event loop: failed to add fd %d to epoll (%s)", fd, strerror(errno));
-        event_loop_quit(loop, ret);
+        struct epoll_event epoll_event;
+        epoll_event.events = EPOLLIN;
+        epoll_event.data.fd = fd;
+        if (epoll_ctl(loop->epoll_fd, EPOLL_CTL_ADD, fd, &epoll_event) < 0) {
+            int ret = -errno;
+            err("event loop: failed to add fd %d to epoll (%s)", fd, strerror(errno));
+            event_loop_quit(loop, ret);
+            goto out;
+        }
+
+        new_item = xcalloc(1, sizeof(*new_item));
+        new_item->loop = loop;
+        new_item->fd = fd;
+        new_item->priority = -1;
+        new_item->callback = callback;
+        new_item->data = data;
+
+        spa_list_insert(&loop->items, &new_item->link);
+    } else {
+        int priority = -fd;
+        debug("event loop: adding unconditional callback with prio %d to event loop", priority);
+
+        new_item = xcalloc(1, sizeof(*new_item));
+        new_item->loop = loop;
+        new_item->fd = -1;
+        new_item->priority = priority;
+        new_item->callback = callback;
+        new_item->data = data;
+
+        if (spa_list_is_empty(&loop->unconditional_items)) {
+            spa_list_insert(&loop->unconditional_items, &new_item->link);
+        } else {
+            struct event_loop_item *elem;
+            spa_list_for_each_reverse(elem, &loop->unconditional_items, link) {
+                /*         |6|
+                 * |9|  |8|\/|4|  |2|
+                 * <-----------------
+                 * iterate from the end and find the first item with higher prio
+                 */
+                bool found = false;
+                if (elem->priority > priority) {
+                    found = true;
+                    spa_list_insert(&elem->link, &new_item->link);
+                }
+                if (!found) {
+                    spa_list_insert(&loop->unconditional_items, &new_item->link);
+                }
+            }
+        }
     }
 
-    spa_list_insert(&loop->items, &new_item->link);
-
+out:
     return new_item;
 }
 
 void event_loop_remove_item(struct event_loop_item *item) {
-    debug("event loop: removing fd %d from event loop", item->fd);
+    if (item->fd >= 0) {
+        debug("event loop: removing fd %d from event loop", item->fd);
+
+        if (fd_is_valid(item->fd)) {
+            if (epoll_ctl(item->loop->epoll_fd, EPOLL_CTL_DEL, item->fd, NULL) < 0) {
+                int ret = -errno;
+                err("event loop: failed to remove fd %d from epoll (%s)",
+                    item->fd, strerror(errno));
+                event_loop_quit(item->loop, ret);
+            }
+            close(item->fd);
+        } else {
+            warn("event loop: fd %d is not valid, was it closed somewhere else?", item->fd);
+        }
+    } else {
+        debug("event loop: removing unconditional callback with prio %d from event loop",
+              item->priority);
+    }
 
     spa_list_remove(&item->link);
-
-    if (fd_is_valid(item->fd)) {
-        if (epoll_ctl(item->loop->epoll_fd, EPOLL_CTL_DEL, item->fd, NULL) < 0) {
-            int ret = errno;
-            err("event loop: failed to remove fd %d from epoll (%s)", item->fd, strerror(errno));
-            event_loop_quit(item->loop, ret);
-        }
-        close(item->fd);
-    } else {
-        warn("event loop: fd %d is not valid, was it closed somewhere else?", item->fd);
-    }
 
     free(item);
 }
@@ -136,27 +172,6 @@ struct event_loop *event_loop_item_get_loop(struct event_loop_item *item) {
 
 int event_loop_item_get_fd(struct event_loop_item *item) {
     return item->fd;
-}
-
-struct event_loop_uncond *event_loop_add_uncond(struct event_loop *loop,
-                                                event_loop_uncond_callback_t callback, void *data) {
-    debug("event loop: adding unconditional callback");
-
-    struct event_loop_uncond *new_item = xcalloc(1, sizeof(*new_item));
-    new_item->callback = callback;
-    new_item->data = data;
-
-    spa_list_insert(&loop->uncond_callbacks, &new_item->link);
-
-    return new_item;
-}
-
-void event_loop_remove_uncond(struct event_loop_uncond *uncond) {
-    debug("event loop: removing unconditional callback");
-
-    spa_list_remove(&uncond->link);
-
-    free(uncond);
 }
 
 int event_loop_run(struct event_loop *loop) {
@@ -192,7 +207,7 @@ int event_loop_run(struct event_loop *loop) {
                     match_found = true;
                     ret = item->callback(item->data, item);
                     if (ret < 0) {
-                        err("event loop: callback returned non-zero, quitting");
+                        err("event loop: callback returned negative, quitting");
                         loop->retcode = ret;
                         goto out;
                     }
@@ -201,16 +216,6 @@ int event_loop_run(struct event_loop *loop) {
 
             if (!match_found) {
                 warn("event loop: no handlers were found for fd %d (BUG)", events[n].data.fd);
-            }
-        }
-
-        struct event_loop_uncond *uncond, *uncond_tmp;
-        spa_list_for_each_safe(uncond, uncond_tmp, &loop->uncond_callbacks, link) {
-            ret = uncond->callback(uncond->data, uncond);
-            if (ret < 0) {
-                err("event loop: unconditional callback returned non-zero, quitting");
-                loop->retcode = ret;
-                goto out;
             }
         }
     }
--- src/event_loop.h
+++ src/event_loop.h
@@ -8,10 +8,18 @@ struct event_loop_uncond;
 typedef int (*event_loop_callback_t)(void *data, struct event_loop_item *loop_item);
 typedef int (*event_loop_uncond_callback_t)(void *data, struct event_loop_uncond *uncond);
 
-/* returns NULL on failure */
+/* creates a new event_loop instance. returns NULL on failure */
 struct event_loop *event_loop_create(void);
+/* frees all resources associated with the loop. loop is not valid after this function returns. */
 void event_loop_cleanup(struct event_loop *loop);
 
+/*
+ * Add new callback to event loop.
+ * If fd >= 0, fd is added to epoll interest list.
+ * If fd < 0, callback will run unconditionally on every event loop iteration.
+ *   Callbacks with higher priority will run before callbacks with lower priority.
+ *   If two callbacks have equal priority, the order is undefined.
+ */
 struct event_loop_item *event_loop_add_item(struct event_loop *loop, int fd,
                                             event_loop_callback_t callback, void *data);
 void event_loop_remove_item(struct event_loop_item *item);
