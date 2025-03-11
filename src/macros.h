6aa79103e43df84705050b25587c77dd1be3a074
heather7283 <heather7283@protonmail.com>
2025-03-11 23:32:16 +0400

    refactor: nuke unused macros

--- src/macros.h
+++ src/macros.h
@@ -1,17 +1,7 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define ANSI_DIM    "\033[2m"
-#define ANSI_RED    "\033[031m"
-#define ANSI_YELLOW "\033[033m"
-#define ANSI_RESET  "\033[0m"
-
 #define streq(a, b) (strcmp(a, b) == 0)
-
-#define min(a, b) a < b ? a : b
-#define max(a, b) a > b ? a : b
-
-#define UNUSED(var) ((void)(var)) /* https://stackoverflow.com/a/3599170 */
 
 #define BYTE_BINARY_FORMAT "0b%c%c%c%c%c%c%c%c"
 #define BYTE_BINARY_ARGS(byte) \
