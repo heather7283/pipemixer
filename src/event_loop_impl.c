89143f43c0f3a8626ab69eb5e1803f854ec53dea
heather7283 <heather7283@protonmail.com>
2025-03-17 13:39:48 +0400

    chore: update event_loop.h

--- src/event_loop_impl.c
+++ src/event_loop_impl.c
@@ -4,7 +4,8 @@
 #define EVENT_LOOP_FREE(ptr) free(ptr)
 
 #include "log.h"
-#define EVENT_LOOP_LOG_DEBUG(fmt, ...) debug("event loop: " fmt, ##__VA_ARGS__)
+//#define EVENT_LOOP_LOG_DEBUG(fmt, ...) debug("event loop: " fmt, ##__VA_ARGS__)
+#define EVENT_LOOP_LOG_INFO(fmt, ...) info("event loop: " fmt, ##__VA_ARGS__)
 #define EVENT_LOOP_LOG_WARN(fmt, ...) warn("event loop: " fmt, ##__VA_ARGS__)
 #define EVENT_LOOP_LOG_ERR(fmt, ...) err("event loop: " fmt, ##__VA_ARGS__)
 
