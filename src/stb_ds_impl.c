2c8e66cda349b909441ea808fa233ce077e82c7d
heather7283 <heather7283@protonmail.com>
2025-03-19 21:50:07 +0400

    style: consistent include style

--- src/stb_ds_impl.c
+++ src/stb_ds_impl.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-
 #include "xmalloc.h"
-
 #define STBDS_REALLOC(context, ptr, size) xrealloc(ptr, size)
 #define STBDS_FREE(context, ptr) free(ptr)
-#define STBDS_NO_SHORT_NAMES
-#define STB_DS_IMPLEMENTATION
 
+#define STBDS_NO_SHORT_NAMES
+
+#define STB_DS_IMPLEMENTATION
 #include "thirdparty/stb_ds.h"
 
