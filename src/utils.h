cd68cd1aa6d0fc6ebcb9cca25c2e98959f288a60
heather7283 <heather7283@protonmail.com>
2025-04-01 13:55:00 +0400

    refactor: remove dead code

--- src/utils.h
+++ src/utils.h
@@ -1,13 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <stdbool.h>
 #include <wchar.h>
 #include <spa/param/audio/raw.h>
 
 const char *channel_name_from_enum(enum spa_audio_channel chan);
-
-wchar_t *mbstowcsdup(const char *src);
 
 /* modifies string in place! */
 size_t wcstrimcols(wchar_t *str, size_t col);
