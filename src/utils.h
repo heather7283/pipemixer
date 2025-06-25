1b1c461459ee7c8becd6b93b12a442e12fd3f9ca
heather7283 <heather7283@protonmail.com>
2025-06-26 00:10:13 +0400

    fix(utils): specify correct parameter type

--- src/utils.h
+++ src/utils.h
@@ -12,7 +12,7 @@ const char *key_name_from_key_code(wint_t code);
 bool key_code_from_key_name(const char *name, wint_t *keycode);
 
 bool str_to_ulong(const char *str, unsigned long *res);
-bool str_to_long(const char *str, unsigned long *res);
+bool str_to_long(const char *str, long *res);
 bool str_to_u32(const char *str, uint32_t *res);
 bool str_to_i32(const char *str, int32_t *res);
 
