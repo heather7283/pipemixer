0feda42589e45e679e0e697e87d44c0bf55b7fa4
heather7283 <heather7283@protonmail.com>
2025-09-19 12:41:56 +0400

    fix: include necessary headers for compilation on void linux
    fixes #20

--- src/pw/node.c
+++ src/pw/node.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 
 #include <spa/pod/builder.h>
 #include <spa/param/props.h>
--- src/tui.c
+++ src/tui.c
@@ -1,3 +1,4 @@
+#include <sys/ioctl.h>
 #include <assert.h>
 #include <math.h>
 #include <wchar.h>
