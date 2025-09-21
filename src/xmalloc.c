2cff6961fb0cd76fce51976bee2559fb1e088b47
heather7283 <heather7283@protonmail.com>
2025-09-21 22:55:48 +0400

    refactor: make xmalloc functions more clean

--- src/xmalloc.c
+++ src/xmalloc.c
@@ -4,61 +4,36 @@
 
 #include "xmalloc.h"
 
-static void handle_alloc_failure(void) {
-    fprintf(stderr, "memory allocation failed, buy more ram lol\n");
-    fflush(stderr);
-    abort();
+static void *check_alloc(void *const alloc) {
+    if (alloc == NULL) {
+        fprintf(stderr, "memory allocation failed, buy more ram lol\n");
+        fflush(stderr);
+        abort();
+    }
+    return alloc;
 }
 
 void *xmalloc(size_t size) {
-    void *alloc = malloc(size);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return alloc;
+    return check_alloc(malloc(size));
 }
 
 void *xzalloc(size_t size) {
-    void *alloc = malloc(size);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return memset(alloc, '\0', size);
+    return memset(check_alloc(malloc(size)), '\0', size);
 }
 
 void *xcalloc(size_t n, size_t size) {
-    void *alloc = calloc(n, size);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return alloc;
+    return check_alloc(calloc(n, size));
 }
 
 void *xrealloc(void *ptr, size_t size) {
-    void *alloc = realloc(ptr, size);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return alloc;
+    return check_alloc(realloc(ptr, size));
 }
 
 void *xreallocarray(void *ptr, size_t nmemb, size_t size) {
-    void *alloc = reallocarray(ptr, nmemb, size);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return alloc;
+    return check_alloc(reallocarray(ptr, nmemb, size));
 }
 
 char *xstrdup(const char *s) {
-    if (s == NULL) {
-        return NULL;
-    }
-
-    char *alloc = strdup(s);
-    if (alloc == NULL) {
-        handle_alloc_failure();
-    }
-    return alloc;
+    return (s == NULL) ? NULL : check_alloc(strdup(s));
 }
 
--- src/xmalloc.h
+++ src/xmalloc.h
@@ -6,7 +6,7 @@
 
 /* malloc, but aborts on alloc fail */
 void *xmalloc(size_t size);
-/* xmalloc but zero initialises memory */
+/* xmalloc that also zero initialises memory */
 void *xzalloc(size_t size);
 /* calloc, but aborts on alloc fail */
 void *xcalloc(size_t n, size_t size);
@@ -15,7 +15,7 @@ void *xrealloc(void *ptr, size_t size);
 /* reallocarray, but aborts on alloc fail */
 void *xreallocarray(void *ptr, size_t nmemb, size_t size);
 
-/* strdup, but aborts on alloc fail and returns NULL when called with NULL */
+/* strdup, but aborts on alloc fail and returns NULL when called on NULL */
 char *xstrdup(const char *s);
 
 #endif /* #ifndef XMALLOC_H */
