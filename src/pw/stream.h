0d369a4791630c6bfe9dff9da2d837a645846723
heather7283 <heather7283@protonmail.com>
2025-10-24 18:24:28 +0400

    feat(pw): listen on nodes and record peak values

---
+++ src/pw/stream.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <pipewire/pipewire.h>
+#include <spa/param/audio/format-utils.h>
+
+#include <stdbool.h>
+
+typedef void (*stream_peak_callback_t)(float peaks[], int npeaks, void *data);
+
+struct stream {
+    struct pw_stream *pw_stream;
+    struct spa_hook listener;
+
+    struct spa_audio_info format;
+
+    stream_peak_callback_t callback;
+    void *callback_data;
+};
+
+bool stream_create(struct stream *stream, uint64_t node_serial,
+                   stream_peak_callback_t callback, void *callback_data);
+
+void stream_destroy(struct stream *stream);
+
