a7ac746842c7b5a3b49319a4a4802f001a907b33
heather7283 <heather7283@protonmail.com>
2026-02-15 22:30:35 +0400

    refactor(log): cleanup logging macros

--- src/log.h
+++ src/log.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define PRINTF(fmt_index, first_arg_index) \
     __attribute__((format(printf, fmt_index, first_arg_index)))
@@ -21,42 +22,19 @@ enum log_loglevel log_str_to_loglevel(const char *str);
 void log_init(FILE *stream, enum log_loglevel level, bool force_colors);
 PRINTF(2, 3) void log_print(enum log_loglevel level, char *msg, ...);
 
-/* ncurses has a trace macro so make this one uppercase, TODO: make others uppercase too */
-#define TRACE(fmt, ...) \
-    do { \
-        log_print(LOG_TRACE, \
-                  "%s:%-3d " fmt, \
-                  __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
+#define TRACE(fmt, ...) log_print(LOG_TRACE, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define DEBUG(fmt, ...) log_print(LOG_DEBUG, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define INFO(fmt, ...) log_print(LOG_INFO, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define WARN(fmt, ...) log_print(LOG_WARN, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define WARN(fmt, ...) log_print(LOG_WARN, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define ERROR(fmt, ...) log_print(LOG_ERROR, "%s:%-3d " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define DEBUG(fmt, ...) \
-    do { \
-        log_print(LOG_DEBUG, \
-                  "%s:%-3d " fmt, \
-                  __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
-
-#define INFO(fmt, ...) \
-    do { \
-        log_print(LOG_INFO, \
-                  "%s:%-3d " fmt, \
-                  __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
-
-
-#define WARN(fmt, ...) \
-    do { \
-        log_print(LOG_WARN, \
-                  "%s:%-3d " fmt, \
-                  __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
-
-#define ERROR(fmt, ...) \
-    do { \
-        log_print(LOG_ERROR, \
-                  "%s:%-3d " fmt, \
-                  __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
+#define ASSERT(expr) ({ \
+    if (!(expr)) { \
+        ERROR("assertion failed in %s: %s", __func__, #expr); \
+        abort(); \
+    } \
+})
 
 #undef PRINTF
 
