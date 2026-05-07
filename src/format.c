40827b849d71c40d7e25f82979bf3ca6a4caa07c
heather7283 <heather7283@protonmail.com>
2026-05-07 21:12:47 +0400

    refactor(format): deal with potential memleaks

--- src/format.c
+++ src/format.c
@@ -26,8 +26,7 @@ struct parser {
     longjmp((pp)->jmp_buf, 67); \
 } while (0)
 
-static struct format_node *parse_node(struct parser *p);
-static void format_node_free_contents(struct format_node *node);
+static void parse_node(struct parser *p, struct format_node **out);
 
 static bool eof(struct parser *p) {
     return p->pos >= p->len && !p->has_wchar;
@@ -77,73 +76,57 @@ static void expect(struct parser *p, wchar_t expected) {
     }
 }
 
-static struct string parse_key(struct parser *p) {
-    struct string s = {0};
-
-    wchar_t c;
+static void parse_key(struct parser *p, struct string *out) {
     while (!eof(p)) {
-        c = peek(p);
+        wchar_t c = peek(p);
         if (!(c >= L'a' && c <= L'z') || c == L'.') {
             break;
         }
-        string_append(&s, consume(p));
+        string_append(out, consume(p));
     }
 
-    if (!s.len) {
-        string_free(&s);
+    if (!out->len) {
         if (eof(p)) {
             PARSER_ERROR(p, "unexpected EOF, expected key");
         } else {
-            PARSER_ERROR(p, "expected key, got %lc", c);
+            PARSER_ERROR(p, "expected key, got %lc", peek(p));
         }
     }
-
-    return s;
 }
 
-static struct format_node *parse_subst(struct parser *p) {
+static void parse_subst(struct parser *p, struct format_node **out) {
+    struct format_node *n = *out = xzalloc(sizeof(*n));
+    n->type = FORMAT_NODE_SUBST;
+
     expect(p, L'{');
 
-    struct string key = parse_key(p);
+    parse_key(p, &n->as.subst.key);
 
-    struct format_node *if_true = NULL;
     if (peek(p) == L'?') {
         consume(p);
-        if_true = parse_node(p);
+        parse_node(p, &n->as.subst.if_true);
     }
 
-    struct format_node *if_false = NULL;
     if (peek(p) == L'!') {
         consume(p);
-        if_false = parse_node(p);
+        parse_node(p, &n->as.subst.if_false);
     }
 
-    // TODO: this leaks on failure
     expect(p, L'}');
-
-    struct format_node *n = xmalloc(sizeof(*n));
-    *n = (struct format_node){
-        .type = FORMAT_NODE_SUBST,
-        .as.subst = {
-            .key = key,
-            .if_true = if_true,
-            .if_false = if_false,
-        },
-    };
-
-    return n;
 }
 
 static bool is_escapable(wchar_t c) {
     return ((c == L'{') || (c == L'}') || (c == L'?') || (c == L'!') || (c == L'\\'));
 }
 
-static struct format_node *parse_literal(struct parser *p) {
-    struct wstring s = {0};
+static void parse_literal(struct parser *p, struct format_node **out) {
+    struct format_node *n = *out = xzalloc(sizeof(*n));
+    n->type = FORMAT_NODE_LITERAL;
 
-    wchar_t c;
+    struct wstring *s = &n->as.literal.str;
+
     while (!eof(p)) {
-        c = peek(p);
+        wchar_t c = peek(p);
         if (c == L'\\') {
             consume(p);
             c = peek(p);
@@ -156,55 +139,33 @@ static struct format_node *parse_literal(struct parser *p) {
             break;
         }
 
-        wstring_append(&s, consume(p));
+        wstring_append(s, consume(p));
     }
 
-    if (!s.len) {
-        wstring_free(&s);
+    if (!s->len) {
+        wstring_free(s);
         if (eof(p)) {
             PARSER_ERROR(p, "unexpected EOF, expected literal");
         } else {
-            PARSER_ERROR(p, "expected literal, got %lc", c);
+            PARSER_ERROR(p, "expected literal, got %lc", peek(p));
         }
     }
-
-    struct format_node *n = xmalloc(sizeof(*n));
-    *n = (struct format_node){
-        .type = FORMAT_NODE_LITERAL,
-        .as.literal = {
-            .str = s,
-        },
-    };
-
-    return n;
 }
 
-static struct format_node *parse_node(struct parser *p) {
+static void parse_node(struct parser *p, struct format_node **out) {
     if (peek(p) == L'{') {
-        return parse_subst(p);
+        parse_subst(p, out);
     } else {
-        return parse_literal(p);
+        parse_literal(p, out);
     }
 }
 
-static void format_node_free_contents(struct format_node *node) {
-    if (!node) {
-        return;
-    }
+static void parse_format(struct parser *p, struct format **out) {
+    struct format *f = *out = xzalloc(sizeof(*f));
 
-    switch (node->type) {
-    case FORMAT_NODE_LITERAL:
-        wstring_free(&node->as.literal.str);
-        break;
-    case FORMAT_NODE_SUBST:
-        string_free(&node->as.subst.key);
-        struct format_node *a = node->as.subst.if_true;
-        struct format_node *b = node->as.subst.if_false;
-        format_node_free_contents(a);
-        format_node_free_contents(b);
-        free(a);
-        free(b);
-        break;
+    while (!eof(p)) {
+        f = *out = xrealloc(f, sizeof(*f) + sizeof(f->nodes[0]) * ++f->nodes_count);
+        parse_node(p, &f->nodes[f->nodes_count - 1]);
     }
 }
 
@@ -214,7 +175,7 @@ struct format *format_parse(const char *src, char **error) {
         .len = strlen(src),
     };
 
-    struct format *f = xzalloc(sizeof(*f));
+    struct format *f = NULL;
 
     if (setjmp(p.jmp_buf)) {
         /* abnormal return */
@@ -225,11 +186,7 @@ struct format *format_parse(const char *src, char **error) {
         return NULL;
     }
 
-    while (!eof(&p)) {
-        struct format_node *node = parse_node(&p);
-        f = xrealloc(f, sizeof(*f) + sizeof(f->nodes[0]) * ++f->nodes_count);
-        f->nodes[f->nodes_count - 1] = node;
-    }
+    parse_format(&p, &f);
 
     if (error) {
         *error = NULL;
@@ -281,6 +238,27 @@ void format_render(const struct format *fmt, const struct dict *dict, struct wst
     }
 }
 
+static void format_node_free(struct format_node *node) {
+    if (!node) {
+        return;
+    }
+
+    switch (node->type) {
+    case FORMAT_NODE_LITERAL:
+        wstring_free(&node->as.literal.str);
+        break;
+    case FORMAT_NODE_SUBST:
+        string_free(&node->as.subst.key);
+        struct format_node *a = node->as.subst.if_true;
+        struct format_node *b = node->as.subst.if_false;
+        format_node_free(a);
+        format_node_free(b);
+        break;
+    }
+
+    free(node);
+}
+
 void format_free(struct format *fmt) {
     if (!fmt) {
         return;
@@ -288,7 +266,7 @@ void format_free(struct format *fmt) {
 
     for (unsigned i = 0; i < fmt->nodes_count; i++) {
         struct format_node *node = fmt->nodes[i];
-        format_node_free_contents(node);
+        format_node_free(node);
     }
     free(fmt);
 }
