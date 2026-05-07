580f18f2b233fd1c9676cec3afe4caf052b0943f
heather7283 <heather7283@protonmail.com>
2026-05-08 01:32:52 +0400

    refactor(format): move implementation details from header to source

--- src/format.c
+++ src/format.c
@@ -6,7 +6,54 @@
 #include <stdio.h>
 
 #include "format.h"
+#include "collections/string.h"
 #include "xmalloc.h"
+
+/*
+ * Grammar:
+ * escapable = "{" | "}" | "?" | "!" | "\" ;
+ * literal_char = ? Any printable unicode character except escapable ? | ( "\" escapable ) ;
+ * literal = literal_char { literal_char } ;
+ *
+ * key_char = ? Any lowercase English letter ? | "." ;
+ * key = key_char { key_char } ;
+ *
+ * subst = "{" key [ "?" [ format ] ] [ "!" format ] "}" ;
+ * node = subst | literal ;
+ * format = node { node } ;
+ */
+
+enum format_node_type {
+    FORMAT_NODE_LITERAL,
+    FORMAT_NODE_SUBST,
+};
+
+enum format_node_subst_type {
+    FORMAT_NODE_SUBST_BASIC, /* {key} */
+    FORMAT_NODE_SUBST_IF_EXISTS, /* {key?then} */
+    FORMAT_NODE_SUBST_IF_ABSENT, /* {key!else} */
+    FORMAT_NODE_SUBST_TERNARY, /* {key?then!else} */
+    FORMAT_NODE_SUBST_FALLBACK, /* {key?!else} */
+};
+
+struct format_node {
+    enum format_node_type type;
+    union {
+        struct {
+            struct wstring str;
+        } literal;
+        struct format_node_subst {
+            struct string key;
+            enum format_node_subst_type type;
+            struct format *if_true, *if_false;
+        } subst;
+    } as;
+};
+
+struct format {
+    unsigned nodes_count;
+    struct format_node *nodes[];
+};
 
 struct parser {
     const char *src;
--- src/format.h
+++ src/format.h
@@ -1,54 +1,7 @@
 #pragma once
 
 #include "collections/dict.h"
-#include "collections/string.h"
 #include "collections/wstring.h"
-
-/*
- * Grammar:
- * escapable = "{" | "}" | "?" | "!" | "\" ;
- * literal_char = ? Any printable unicode character except escapable ? | ( "\" escapable ) ;
- * literal = literal_char { literal_char } ;
- *
- * key_char = ? Any lowercase English letter ? | "." ;
- * key = key_char { key_char } ;
- *
- * subst = "{" key [ "?" [ format ] ] [ "!" format ] "}" ;
- * node = subst | literal ;
- * format = node { node } ;
- */
-
-enum format_node_type {
-    FORMAT_NODE_LITERAL,
-    FORMAT_NODE_SUBST,
-};
-
-enum format_node_subst_type {
-    FORMAT_NODE_SUBST_BASIC, /* {key} */
-    FORMAT_NODE_SUBST_IF_EXISTS, /* {key?then} */
-    FORMAT_NODE_SUBST_IF_ABSENT, /* {key!else} */
-    FORMAT_NODE_SUBST_TERNARY, /* {key?then!else} */
-    FORMAT_NODE_SUBST_FALLBACK, /* {key?!else} */
-};
-
-struct format_node {
-    enum format_node_type type;
-    union {
-        struct {
-            struct wstring str;
-        } literal;
-        struct format_node_subst {
-            struct string key;
-            enum format_node_subst_type type;
-            struct format *if_true, *if_false;
-        } subst;
-    } as;
-};
-
-struct format {
-    unsigned nodes_count;
-    struct format_node *nodes[];
-};
 
 struct format *format_parse(const char *src, char **error);
 void format_render(const struct format *format, const struct dict *dict, struct wstring *result);
